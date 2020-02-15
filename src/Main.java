5406baa62969df3de811ae99feca772fd7106a06
Lucas Samuel <lucasskluser@outlook.com>
2020-02-15 00:19:43 -0300

    docs(Main.java): add documentação

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 import listas.terceira.TerceiraLista;
 import java.io.IOException;
 
+/**
+ * Classe principal de todo o projeto
+ */
 public class Main {
+    /**
+     * Método invocado ao executar o projeto
+     */
     public static void main(String[] args) throws IOException {
         TerceiraLista.run();
     }
